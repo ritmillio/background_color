136c3a76981dbd5668e1d03971492d8fa67fee96
Zoltan Fodor <40596749+ritmillio@users.noreply.github.com>
2018-12-13 11:00:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+
+const body = document.getElementById("gradient");
+
+function setGradient(){
+    body.style.background = "linear-gradient(to right ," + color1.value + "," + color2.value + ")";
+    css.textContent = "Here's your awesome linear-background color: " + body.style.background + ";"; 
+}
+
+/*color1.addEventListener("input" , function(){
+    body.style.background = "linear-gradient(to right ," + color1.value + "," + color2.value + ")";
+})*/
+/*color2.addEventListener("input" , function(){
+    body.style.background = "linear-gradient(to right ," + color1.value + "," + color2.value + ")";
+})*/
+
+color1.addEventListener("input" , setGradient);
+
+color2.addEventListener("input" , setGradient); 
